10f70067aa823473a89e9e4b3438f1d8c2de213c
staylor11x <scott2013taylor@gmail.com>
2023-11-09 11:08:54 +0000

    Git ignore populated

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using static System.Console;
+
+WriteLine("Working with large integers");
+WriteLine("----------------------------");
+
+ulong big =  ulong.MaxValue;
+WriteLine($"{big,40:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+
+WriteLine($"{bigger,40:N0}");
