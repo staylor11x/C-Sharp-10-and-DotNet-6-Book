226eafc7ac26e8fb6ac6ffb3358424e651b2b95d
staylor11x <scott2013taylor@gmail.com>
2023-11-09 13:57:46 +0000

    Chapter08 working with Text and Time

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using static System.Console;
+
+string city = "London";
+WriteLine($"{city} is {city.Length} charcters long");
+WriteLine($"The first char is {city[0]} and the third is {city[2]}");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellin";
+
+string[] citiesArray = cities.Split(',');
+
+WriteLine($"There are {citiesArray.Length} items in the array.");
+foreach(string item in citiesArray)
+{
+    WriteLine(item);
+}
+
+string fullName = "Alan Jones";
+int indexOfTheSpace = fullName.IndexOf(' ');
+
+string firstName = fullName.Substring(0, indexOfTheSpace);
+
+string lastName = fullName.Substring(indexOfTheSpace + 1);
+
+WriteLine($"Original: {fullName}");
+WriteLine($"Swapped: {lastName}, {firstName}");
+
+string company = "Microsoft";
+bool startsWithM = company.StartsWith('M');
+bool containsN = company.Contains("N");
+WriteLine($"Text: {company}");
+WriteLine($"Starts with M: {startsWithM}, contains an N: {containsN}");
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
+
+string fruit = "Apples";
+decimal price = 0.39M;
+DateTime when = DateTime.Today;
+
+//using the + operator, or string.Concat to concatonate strings is bad practice as .NET must create a completley new string in memory!!!
+
+WriteLine($"Ineterpolated: {fruit} cost {price:C} on {when:dddd}");
+WriteLine(string.Format("stirng.Format: {0} cost {1:C} on {2:dddd}.",
+    arg0: fruit, arg1: price, arg2: when));
---
+++ Chapter08/WorkingWithTime/Program.cs
@@ -0,0 +1,82 @@
+﻿using static System.Console;
+using System.Globalization;
+
+WriteLine("Earliest datetime value is : {0}",
+    arg0: DateTime.MinValue);
+
+WriteLine("Latest dattime value is: {0}",
+    arg0: DateTime.MaxValue);
+
+WriteLine("UNIX epoch dat/time value is: {0}",
+    arg0: DateTime.UnixEpoch);
+
+WriteLine("Date/TIme value now is {0}",
+    arg0: DateTime.Now);
+
+WriteLine("Date/Time value today is {0}",
+    arg0: DateTime.Today);
+
+DateTime christmas = new(year: 2023, month: 12, day: 25);
+
+WriteLine("Chirstmas {0}", arg0: christmas);    //defualt format
+WriteLine("Christmas {0:dddd, dd MMMM yyyy}", christmas);
+WriteLine("Christmas is in month {0} of the year.", christmas.Month);
+WriteLine("Christmas is the {0} day of the yeas", christmas.DayOfYear);
+WriteLine("Christams {0} is on a {1}.", christmas.Year, christmas.DayOfWeek);
+
+DateTime beforeXmas = christmas.Subtract(TimeSpan.FromDays(12));
+DateTime afterXmas = christmas.AddDays(12);
+
+WriteLine("12 days before christmas is: {0}", beforeXmas);
+WriteLine("12 days after christmas is: {0}", afterXmas);
+
+TimeSpan untillXmas = christmas - DateTime.Now;
+
+WriteLine("There are {0} days and {1} hours untill christmas.", untillXmas.Days, untillXmas.Hours);
+
+WriteLine("There are {0:N0} hours untill Christmas.", untillXmas.TotalHours);
+
+WriteLine("There are {0} weeks untill Christmas", untillXmas.Days / 7);
+
+DateTime kidsWakeUp = new(year: 2023, month: 12, day: 25, hour: 6, minute: 30, second: 0);
+
+WriteLine("Kids wake up on Christmas: {0}", kidsWakeUp);
+WriteLine("The kids woke me up at {0}", kidsWakeUp.ToShortTimeString());
+
+WriteLine("Current culture is: {0}", CultureInfo.CurrentCulture.Name);
+
+string textDate = "4 July 2021";
+DateTime independanceDay = DateTime.Parse(textDate);
+
+WriteLine("Text: {0}, DateTime: {1:d MMMM}", textDate, independanceDay);
+
+textDate = "7/4/2021";
+independanceDay = DateTime.Parse(textDate);
+
+WriteLine("Text: {0}, DateTime: {1:d MMMM}", textDate, independanceDay);
+
+independanceDay = DateTime.Parse(textDate, provider:CultureInfo.GetCultureInfo("en-US"));
+
+WriteLine("Text: {0}, DateTime: {1:d MMMM}", textDate, independanceDay);
+
+
+
+for(int year = 2020; year < 2026; year++)
+{
+    Write($"{year} is a leap year: {DateTime.IsLeapYear(year)}.");
+    WriteLine("There are {0} daye in Feburary {1}.",
+        DateTime.DaysInMonth(year: year, month: 2), year);
+}
+
+WriteLine("Is Christmas daylight saving time? {0},", christmas.IsDaylightSavingTime());
+
+WriteLine("Is July 4th daylight saving time? {0}", independanceDay.IsDaylightSavingTime());
+
+DateOnly queensBirthday = new(year: 2022, month: 4, day: 21);
+WriteLine($"The Queen's birthday is on {queensBirthday}");
+
+TimeOnly partyStarts = new(hour: 20, minute: 30);
+WriteLine($"The Queen's party starts at {partyStarts}");
+
+DateTime calanderEntry = queensBirthday.ToDateTime(partyStarts);
+WriteLine($"Add to your calendar: {calanderEntry}.");
