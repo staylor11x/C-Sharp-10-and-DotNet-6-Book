eb984e2427da0ef71e6dc2b5fe0bc9eb6adee961
staylor11x <scott2013taylor@gmail.com>
2023-11-14 12:57:17 +0000

    Chapter 9, half way done

---
+++ Chapter09/WorkingWithFileSystems/Program.cs
@@ -0,0 +1,124 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+using static System.Formats.Asn1.AsnWriter;
+
+//OutputFileSystemInfo();
+//WorkingWithDrives();
+//WorkingWithDirectories();
+WorkingWithFiles();
+
+
+static void OutputFileSystemInfo()
+{
+    WriteLine("{0,-33} {1}", "Path.PathSeperator", PathSeparator);
+    WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar );
+    WriteLine("{0,-33} {1}", "Path.GetCurrentDirectory()", GetCurrentDirectory());
+    WriteLine("{0,-33} {1}", "Path.CurrentDirectory", CurrentDirectory);
+    WriteLine("{0,-33} {1}", "Path.SystemDirectory", SystemDirectory);
+    WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+
+    WriteLine("GetFolderPath(SpecifiedFolder)");
+    WriteLine("{0,-33} {1}", ".System", SpecialFolder.System);
+    WriteLine("{0,-33} {1}", ".ApplicationData", SpecialFolder.ApplicationData);
+    WriteLine("{0,-33} {1}", ".MyDocuments", SpecialFolder.MyDocuments);
+    WriteLine("{0,-33} {1}", ".Personal", SpecialFolder.Personal);
+}
+
+static void WorkingWithDrives()
+{
+    WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}",
+        "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+
+    foreach(DriveInfo drive in DriveInfo.GetDrives())
+    {
+        if(drive.IsReady)
+        {
+            WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}",
+        drive.Name, drive.DriveType, drive.DriveFormat, drive.TotalSize, drive.AvailableFreeSpace);
+        }
+        else
+        {
+            WriteLine("{0,-30} | {1,-10}", drive.Name, drive.DriveType);
+        }
+    }
+}
+
+static void WorkingWithDirectories()
+{
+    // Define a direcroty path for a new folder
+    // starting in the user's folder
+    string newFolder = Combine(GetFolderPath(SpecialFolder.Personal),"Code", "Chapter09", "NewFolder");
+
+    WriteLine($"Working with: {newFolder}");
+
+    //check if it exists
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+
+    //create directory
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+
+    //delete directory
+    WriteLine("Deleting it...");
+    Delete(newFolder, recursive: true);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+}
+
+static void WorkingWithFiles()
+{
+    // define a directory path to the output files
+    // starting in the user's folder
+
+    string dir = Combine(GetFolderPath(SpecialFolder.UserProfile),"OneDrive","Dev","C# Course","Chapter09","OutputFiles");
+
+    CreateDirectory(dir);
+
+    //define file paths
+    string textFile = Combine(dir, "Dummy.txt");
+    string backupFile = Combine(dir, "Dummy.bak");
+    WriteLine($"Working with: {textFile}");
+
+    // check if file exists
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+    // creat a new file and write a line to it
+    StreamWriter textWriter = File.CreateText( textFile );
+    textWriter.WriteLine("Hello C#!");
+    textWriter.Close(); //close the file and release the resources
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+    // copy the file and overwrite if it already exists
+    File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+    Write("Confirm the files exist, and then press ENTER: ");
+    ReadLine();
+
+    // delete file
+    File.Delete(textFile);
+    WriteLine($"Does the file exist? {File.Exists(textFile)}");
+
+    // read from the text file backup
+    WriteLine($"Reading contents of {backupFile}:");
+    StreamReader textReader = File.OpenText(backupFile);
+    WriteLine(textReader.ReadToEnd());
+    textReader.Close();
+
+    // managing paths
+    WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+    WriteLine($"File Name: {GetFileName(textFile)}");
+    WriteLine($"FIle name without extension: {0}", GetFileNameWithoutExtension(textFile));
+    WriteLine($"FIle Extension: {GetExtension(textFile)}");
+    WriteLine($"Random File Name: {GetRandomFileName()}");
+    WriteLine($"Temporary file name: {GetTempFileName()}");
+
+    FileInfo info = new(backupFile);
+    WriteLine($"{backupFile}:");
+    WriteLine($"Contains {info.Length} bytes");
+    WriteLine($"Last accessed {info.LastAccessTime}");
+    WriteLine($"Has readonly set to {info.IsReadOnly}");
+
+}
---
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,182 @@
+﻿using static System.Console;
+using System.Xml;
+using static System.Environment;
+using static System.IO.Path;
+using System.Xml.Schema;
+using System.IO.Compression;
+
+string currentDirectory = Combine(GetFolderPath(SpecialFolder.UserProfile), "OneDrive", "Dev", "C# Course", "Chapter09","OutputFiles");
+
+//WorkingWithText(currentDirectory);
+WorkingWithXML(currentDirectory);
+WorkingWithCompression(currentDirectory);
+WorkingWithCompression(currentDirectory, useBrotli: false);
+
+static void WorkingWithText(string currentDirectory)
+{
+    //define a file to write to
+    string textFile = Combine(currentDirectory, "Streams.txt");
+
+    // create a text file and return a helpful writer
+    StreamWriter text = File.CreateText(textFile);
+
+    // enumerate the strings, writing each one to the stream on a seperate line
+    foreach(string item in Viper.CallSigns)
+    {
+        text.WriteLine(item);
+    }
+    text.Close();
+
+    // output the contents of the file
+    WriteLine("{0} contains {1:N0} bytes.", textFile, new FileInfo(textFile).Length);
+
+    WriteLine(File.ReadAllText(textFile));
+}
+
+static void WorkingWithXML(string currentDirectory)
+{
+    FileStream? xmlFileStream = null;
+    XmlWriter? xml = null;
+
+    try
+    {
+        //define a file to write to
+        string xmlFile = Combine(currentDirectory, "streams.xml");
+
+        //create a file stream
+        xmlFileStream = File.Create(xmlFile);
+
+        //wrap the file stream in an xml writer helper and automatically indent nested elements
+        xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+        //write the xml declaration
+        xml.WriteStartDocument();
+
+        //write the root element
+        xml.WriteStartElement("Callsigns");
+
+        //enumerate the strings writing each one to the stream
+        foreach (string item in Viper.CallSigns)
+        {
+            xml.WriteElementString("callsign", item);
+        }
+
+        //write the close root element
+        xml.WriteEndElement();
+
+        //close helper and steam 
+        xml.Close();
+        xmlFileStream.Close();
+
+        //output all the elements of the file
+        WriteLine("{0} contains {1:N0} bytes.", xmlFile, new FileInfo(xmlFile).Length);
+
+        WriteLine(File.ReadAllText(xmlFile));
+    }
+    catch(Exception ex)
+    {
+        // if the path dosen't exist the exception will be caught
+        WriteLine($"{ex.GetType()} says {ex.Message}");
+
+    }
+    finally         //this section can be better optimised if you use the 'using' statement to call the writer
+    {
+        if(xml != null)
+        {
+            xml.Dispose();
+            WriteLine("The XML Writer's unmanaged resources have been disposed.");
+        }
+        if(xmlFileStream != null)
+        {
+            xmlFileStream.Dispose();
+            WriteLine("The file stream's unmanaged resources have been disposed.");
+        }
+    }
+}
+
+static void WorkingWithCompression(string currentDirectory, bool useBrotli = true)
+{
+    string fileExt = useBrotli ? "brolti" : "gzip";
+    
+
+    //compress the xml output
+    string filePath = Combine(currentDirectory, $"streams.{fileExt}");
+
+    FileStream file = File.Create(filePath);
+
+    Stream compressor;
+
+    if (useBrotli)
+    {
+        compressor = new BrotliStream(file, CompressionMode.Compress);
+    }
+    else
+    {
+        compressor = new GZipStream(file, CompressionMode.Compress);
+    }
+
+    using (compressor)
+    {
+        using(XmlWriter xml = XmlWriter.Create(compressor))
+        {
+            xml.WriteStartDocument();
+            xml.WriteStartElement("Callsigns");
+
+            foreach(string item in Viper.CallSigns)
+            {
+                xml.WriteElementString("callsign", item);
+            }
+
+            // the normal call to WriteEndElement is not necessary bacuse when the xml writer disposes, it will 
+            // automatically end any elements of any depth
+        }
+    }   //also closes the underlying stream
+
+    //output all the contents of the uncompressed file
+    WriteLine("{0} contains {1:N0} bytes. ", filePath, new FileInfo(filePath).Length);
+
+    WriteLine($"The compressed contents: ");
+    WriteLine(File.ReadAllText(filePath));
+
+    //read the compressed file
+    WriteLine("Reading from the compresesd xml file");
+    file = File.Open(filePath, FileMode.Open);
+
+    Stream decompressor;
+    if (useBrotli)
+    {
+        decompressor = new BrotliStream(file, CompressionMode.Decompress);
+    }
+    else
+    {
+        decompressor = new GZipStream(file, CompressionMode.Decompress);
+    }
+
+    using (decompressor)
+    {
+        using(XmlReader reader = XmlReader.Create(decompressor))
+        {
+            while (reader.Read())   //read the next xml node
+            {
+                //check if we are on an element node named callsign
+                if((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+                {
+                    reader.Read();  //move to the text inside element
+                    WriteLine($"{reader.Value}");   //read its value
+                }
+            }
+        }
+    }
+}
+
+
+
+
+static class Viper
+{
+    // define an array of viper pilot call signs
+    public static string[] CallSigns = new[]
+    {
+        "Husker","Starbuck","Apollo","Boomer","Bulldog","Athena","Helo","Racetrack"
+    };
+}
