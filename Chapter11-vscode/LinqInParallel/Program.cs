0ca190e55dddf4a4dcb39c2bbc7026665d24667a
Scott Taylor <scott2013taylor@gmail.com>
2023-11-21 21:12:16 +0000

    Chapter11 Completed - Excluding Ex2
    	-- Fixed our problem from before turns out it was a typing error
    	-- No further problems
    	-- Interesting to see the mutlicore options using linq

---
+++ Chapter11-vscode/LinqInParallel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+using static System.Console;
+ 
+ Stopwatch watch = new();
+ Write("Press enter to start");
+ ReadLine();
+ watch.Start();
+
+ int max = 45;
+
+ IEnumerable<int> numbers = Enumerable.Range(start: 1, count: max);
+
+ WriteLine($"Calculating Fibonacci sequence up to (max). Please wait...");
+
+ int[] fibonacciNumbers = numbers.AsParallel()          //takes half the time to complete in parallel and all of our logical processors did max out
+    .Select(number => Fibonacci(number))
+    .OrderBy(number => number)
+    .ToArray();
+
+ watch.Stop();
+ WriteLine("{0:#,##0} elapsed milliseconds", watch.ElapsedMilliseconds);
+
+ Write("Results");
+ foreach(int number in fibonacciNumbers)
+ {
+    Write($" {number}");
+ }
+
+ static int Fibonacci(int term) =>      //some basic recursion and a fancy looking function! This is maybe worh remebering seems like something that could come up in a coding challange!
+    term switch
+    {
+        1 => 0,
+        2 => 1,
+        _ => Fibonacci(term - 1) + Fibonacci(term - 2)
+    };
--- Chapter11-vscode/LinqWithEFCore/Program.cs
+++ Chapter11-vscode/LinqWithEFCore/Program.cs
@@ -1,11 +1,18 @@
 ﻿using Packt.Shared;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 using static System.Console;
 using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
 
 //FilterAndSort();
-JoinCategoriesAndProducts();
+//JoinCategoriesAndProducts();
+//GroupJoinCategoriesAndProducts();
+//AggregateProducts();
+//CustomExtensionMethods();
+//OutputProductAsXml();
+ProcessSettings();
 
 static void FilterAndSort()
 {
@@ -13,7 +20,15 @@ static void FilterAndSort()
     {
         DbSet<Product> allProducts = db.Products;
 
-        IQueryable<Product> filteredProducts = allProducts.Where(product => product.UnitPrice < 10M);
+        if(allProducts is null)
+        {
+            WriteLine("No products found");
+            return;
+        }
+
+        IQueryable<Product> processedProducts = allProducts.ProcessSequence();
+
+        IQueryable<Product> filteredProducts = processedProducts.Where(product => product.UnitPrice < 10M);
 
         IOrderedQueryable<Product> sortedAndFilteredProducts = filteredProducts.OrderByDescending(product => product.UnitPrice);
         //This not a good example as the querey gets all the columns from the product table instead of just the three that we need!
@@ -43,14 +58,105 @@ static void JoinCategoriesAndProducts()
         var queryJoin = db.Categories.Join(
             inner:db.Products,
             outerKeySelector: category => category.CategoryId,
-            innerKeySelector: product => product.ProductId,
-            resultSelector: (c,p) =>
+            innerKeySelector: product => product.CategoryId,        //THIS IS WHERE THE MISTAKE WAS!!! we had product.ProductId, how silly...
+            resultSelector: (c, p) =>
                 new {c.CategoryName, p.ProductName, p.ProductId}
-        ).OrderBy(cp => cp.CategoryName);
+                ).OrderBy(cp => cp.CategoryName);
 
         foreach(var item in queryJoin)
         {
             WriteLine("{0}: {1} is in {2}.", item.ProductId, item.ProductName, item.CategoryName);
         }
+    }
+}
+
+static void GroupJoinCategoriesAndProducts()
+{
+    using(NorthWind db = new())
+    {
+        //group all the products by their categories to return 8 matches
+        var queryGroup = db.Categories.AsEnumerable().GroupJoin(        //AsEnumerable prevents a run time error
+            inner: db.Products,
+            outerKeySelector: category => category.CategoryId,
+            innerKeySelector: product => product.CategoryId,
+            resultSelector: (c, matchingProducts) => new{
+                c.CategoryName,
+                Products = matchingProducts.OrderBy(p => p.ProductName)
+            });
+
+        foreach(var category in queryGroup)
+        {
+            WriteLine("{0} has  {1} products", category.CategoryName, category.Products.Count());
+
+            foreach(var product in category.Products)
+            {
+                WriteLine($"    {product.ProductName}");
+            }
+        }
+    }
+}
+
+static void AggregateProducts()
+{
+    using(NorthWind db = new())
+    {
+        WriteLine("{0,-25} {1,-10}", "Product Count", db.Products.Count());
+
+        WriteLine("{0,-25} {1,10:$#,##0.00}", "Highest Product Price: ", db.Products.Max(p => p.UnitPrice));
+
+        WriteLine("{0,-25} {1,10:N0}", "Sum of units in stock: ", db.Products.Sum(p => p.UnitsInStock));
+
+        WriteLine("{0,-25} {1,10:N0}", "Sum of units on order", db.Products.Sum(p => p.UnitsOnOrder));
+
+        WriteLine("{0,-25} {1,10:$#.##0.00}", "Average unit price", db.Products.Average(p => p.UnitPrice));
+
+        WriteLine("{0,-25} {1,10:$#,##0.00}", "Value of units in stock", db.Products.Sum(p => p.UnitsInStock * p.UnitPrice));
+    }
+}
+
+static void CustomExtensionMethods()
+{
+    using(NorthWind db = new())
+    {
+        WriteLine("Mean units in stock: {0:N0}", db.Products.Average(p => p.UnitsInStock));
+        WriteLine("Mean unit price: {0:$#,##0.00}", db.Products.Average(p => p.UnitPrice));
+        WriteLine("Median units in stock: {0:N0}", db.Products.Median(p => p.UnitsInStock));
+        WriteLine("Median unit price: {0:$#,##0.00}", db.Products.Median(p => p.UnitPrice));
+        WriteLine("Mode units in stock: {0:N0}", db.Products.Mode(p => p.UnitsInStock));
+        WriteLine("Mode unit price: {0:$#,##0.00}", db.Products.Mode(p => p.UnitPrice));
+    }
+}
+
+static void OutputProductAsXml()
+{
+    using(NorthWind db = new())
+    {
+        Product[] productsArray = db.Products.ToArray();
+
+        XElement xml = new("products", from p in productsArray 
+            select new XElement("product", 
+            new XAttribute("id", p.ProductId),
+            new XAttribute("price", p.UnitPrice),
+            new XElement("name", p.ProductName)));
+
+        WriteLine(xml.ToString());
+    }
+}
+
+static void ProcessSettings()
+{
+    XDocument doc = XDocument.Load("settings.xml");
+
+    var appSettings = doc.Descendants("appSettings")
+        .Descendants("add")
+        .Select(node => new
+        {
+            key = node.Attribute("key")?.Value,
+            Value = node.Attribute("value")?.Value
+        }).ToArray();
+    
+    foreach(var item in appSettings)
+    {
+        WriteLine($"{item.key}: {item.Value}");
     }
 }
