55a747f801e2e39e61e77c1709a031d77b4064c0
Scott Taylor <scott2013taylor@gmail.com>
2023-11-24 12:48:38 +0000

    Chapter 13 Completed.
    	--Quite a short chapter in terms of code
    	--Just set up SQLite and SQLServer connections
    	--Also learned about the different types of ways to build
    	applications with .NET and all the various versions avaliable.
    	--Looking forward to the next chapters!

---
+++ PracticalApps/Northwind.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;    //use SqlServer
+using Microsoft.Extensions.DependencyInjection; //IServiceCollection
+
+namespace Packt.Shared;
+
+public static class NorthwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the SqlServer database provider.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="collectionString">Set to override the default.</param>
+    /// <returns>An IServiceCollection that can be used to add more services.</returns>
+    /// 
+
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string connectionString = "Data Source=.;Initial Catalog=Northwind;Integrated Security=true;MultipleActiveResultSets=true;")
+    {
+        services.AddDbContext<NorthwindContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}
---
+++ PracticalApps/Northwind.Common.DataContext.Sqlite/NorhtwindContextExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;    //UseSqlite
+using Microsoft.Extensions.DependencyInjection;     //IServiceCollection
+
+namespace Packt.Shared;
+
+public static class NorhtwindContextExtensions
+{
+    /// <summary>
+    /// Adds NorthwindContext to the specified IServiceCollection. Uses the Sqlite database provider
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="relativePath">Set to override the default of ".."</param>
+    /// <returns>An IServiceColletion that can be used to add more services</returns>
+    
+    public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string relativePath = "..")
+    {
+        string databasePath = Path.Combine(relativePath, "Northwind.db");
+
+        services.AddDbContext<NorthwindContext>(options =>
+            options.UseSqlite($"Data Source={databasePath}")
+            );
+
+        return services;
+    }
+}
