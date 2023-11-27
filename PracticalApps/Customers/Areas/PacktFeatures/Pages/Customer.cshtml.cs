bbcfce485027987140974d0a0ca159de4f4c861b
Scott Taylor <scott2013taylor@gmail.com>
2023-11-27 20:51:31 +0000

    Fixed issue with webpage landing.
    	--The last issue we were facing not being able to load the
    	webpage with the data for a specific supplier - the only problem
    	was we forgot to add the project reference... :#

--- PracticalApps/Customers/Areas/PacktFeatures/Pages/Customer.cshtml.cs
+++ PracticalApps/Customers/Areas/PacktFeatures/Pages/Customer.cshtml.cs
@@ -13,22 +13,22 @@ public class CustomerPageModel : PageModel
         db = injectedContext;
     }
 
-    public Customer Customer { get; set; }
+    public Customer Customer { get; set; } = null!;
 
     public async Task<IActionResult> OnGetAsync(string? id)
     {
-        //if (id is null)
-        //{
-        //    return NotFound();
-        //}
-
+        if (id is null)
+        {
+            return NotFound();
+        }
+    
         Customer = await db.Customers.FindAsync(id);
         Console.WriteLine(Customer.CustomerId);
-        //if(Customer is null)
-        //{
-        //    return NotFound();
-        //}
-
+        if(Customer is null)
+        {
+            return NotFound();
+        }
+    
         return Page();
     }
 }
