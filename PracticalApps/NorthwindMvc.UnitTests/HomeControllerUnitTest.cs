6abacb36112cc9d5d7a481093ababca8890eb783
Scott Taylor <scott2013taylor@gmail.com>
2023-12-01 15:51:56 +0000

    Unit Testing Partially Done:
    	-- Having trouble finishing the unit testing for the home
    	controllers.
    	--Managed to get the bad request one no problem but the returns
    	not found one is causing some issues.
    	--seems to be alot more complicated than it needs to be can just
    	pass in an valid id that dosent correspond to a databse product.
    	i.e. pass in 1000 and it returns a null category because it does
    	not exist. This throws an error...
    	-- Seems to be a little bit of a complex topic on googling all
    	this business about mocking linq and EF queries.
    	-- Hopefully we will have more success on the return, definetly
    	work sticking out and learning.

--- PracticalApps/NorthwindMvc.UnitTests/HomeControllerUnitTest.cs
+++ PracticalApps/NorthwindMvc.UnitTests/HomeControllerUnitTest.cs
@@ -1,6 +1,12 @@
 using Northwind.Mvc.Controllers;
 using Moq;
 using Microsoft.Extensions.Logging;
+using Packt.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
 
 namespace NorthwindMvc.UnitTests
 {
@@ -11,7 +17,92 @@ namespace NorthwindMvc.UnitTests
         {
             //arrange
             var mockLogger = new Mock<ILogger<HomeController>>();
-            var controller = new HomeController(mockLogger);
+            var mockDbContext = new Mock<NorthwindContext>();
+            
+            var controller = new HomeController(mockLogger.Object, mockDbContext.Object);
+
+            int? id = null;
+
+            //Act
+            var result = await controller.CategoryDetail(id);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CategoryDetailReturnsNotFoundWhenCategoryNotFound()
+        {
+            //arrange
+            int? id = 10000;    //id is valid but dosen't exist
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var mockDbContext = new Mock<NorthwindContext>();
+
+            var controller = new HomeController(mockLogger.Object, mockDbContext.Object);
+
+            //Act
+            var result = await controller.CategoryDetail(id);
+
+            //Assert
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ProductDetailReturnsBadRequestWhenIdIsNull()
+        {
+            //arrange
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var mockDbContext = new Mock<NorthwindContext>();
+
+            var controller = new HomeController(mockLogger.Object, mockDbContext.Object);
+
+            int? id = null;
+
+            //act 
+            var result = await controller.ProductDetail(id);
+
+            //assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        //[Fact]
+        //public async Task ProductDetailReturnsNotFoundWhenProductNotFound()
+        //{
+        //    // Arrange
+        //    var mockLogger = new Mock<ILogger<HomeController>>();
+        //    var options = new DbContextOptionsBuilder<NorthwindContext>()
+        //        .UseInMemoryDatabase(databaseName: "TestDatabase")
+        //        .Options;
+        //
+        //    using var dbContext = new NorthwindContext(options);
+        //    var productId = 1;
+        //    dbContext.Products.Add(new Product { ProductId = productId });
+        //    dbContext.SaveChanges();
+        //
+        //    var controller = new HomeController(mockLogger.Object, dbContext);
+        //    //act
+        //    var result = await controller.ProductDetail(id);
+        //
+        //    //assert
+        //    Assert.IsType<NotFoundObjectResult>(result);
+        //}
+
+        [Fact]
+        public async Task ProductDetail_Returns_NotFound_When_ProductNotFound()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var dbContextMock = new Mock<NorthwindContext>();
+            dbContextMock.Setup(db => db.Products.SingleOrDefaultAsync(It.IsAny<Expression<Func<Product, bool>>>(), default))
+                .ReturnsAsync((Product)null);
+
+            var controller = new HomeController(mockLogger.Object,dbContextMock.Object);
+
+            // Act
+            var result = await controller.ProductDetail(999); // Assuming this ID does not exist
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result);
         }
     }
 }
